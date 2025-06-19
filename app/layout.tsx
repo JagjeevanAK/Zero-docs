374670d9fe204317b8aeed6362370d25147770fe
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-06-19 10:47:14 +0530

    added svg
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,10 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="https://0.email/white-icon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="https://0.email/white-icon.svg" type="image/svg+xml" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
